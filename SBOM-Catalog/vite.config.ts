eb842bff63407a08db79a52a195d72a6a9597bfa
mariuxdeangelo <mariuxdeangelo@gmail.com>
2024-04-20 15:49:22 +0200

    fixing routs for static sites

--- SBOM-Catalog/vite.config.ts
+++ SBOM-Catalog/vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.yaml'],
-  base: '/sbom-everywhere/sbom-catalog/',
+  base: '/sbom-everywhere/',
   plugins: [vue()],
 })
