f68d4ddc3f37f593651225f197dc9b60405c5812
mbiebel <mariuxdeangelo@outlook.com>
2025-02-22 18:13:56 +0100

    update images and add suggest fix button for tools
    Signed-off-by: mbiebel <mariuxdeangelo@outlook.com>

--- SBOM-Catalog/src/components/detail-enumeration.vue
+++ SBOM-Catalog/src/components/detail-enumeration.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
-import { ref } from "vue";
 import { useMainStore } from "../stores/mainStore";
 import MarkdownParser from "./markdownParser.vue";
-import type { Tool } from '../types/tool';
 
 const store = useMainStore();
 
@@ -16,23 +14,42 @@ const sourceTooltip = (source: string) => {
   }
 }
 
+const createGitHubIssue = () => {
+  const baseUrl = 'https://github.com/ossf/sbom-everywhere/issues/new';
+  const currentUrl = window.location.href;
+  const title = encodeURIComponent(`Update Entry: ${store.activeSelection}`);
+  const body = encodeURIComponent(`Please update the following entry:\n\nTool: ${store.activeSelection}\nContext URL: ${currentUrl}\n\nSuggested changes:\n\n`);
+  
+  window.open(`${baseUrl}?title=${title}&body=${body}`, '_blank');
+}
 </script>
 
 <template>
   <div class="flex justify-content-between align-items-center m-2">
     <h2 class="m-0">{{ store.activeSelection }}</h2>
+    <div class="flex align-items-center">
       <img
-          :src="'logos/' + store.activeSelection + '.png'" style="display: none"
-          alt=""
-          class="responsive-image p-1 block"/>
+        :src="'logos/' + store.activeSelection + '.png'" 
+        style="display: none"
+        alt=""
+        class="responsive-image p-1 block"
+      />
+    </div>
   </div>
-  <div v-if="store.selectedObject" class="flex justify-content-end flex-wrap">
-    <PChip class="p-1 m-1" v-tooltip="sourceTooltip(store.selectedObject.Source)">{{ store.selectedObject.Source }}</PChip>
-  </div>
-  <PDivider class="mt-2"/>
 
   <div class="scrollable-div">
   <div v-if="store.selectedObject">
+    <div v-if="store.selectedObject" class="flex justify-content-between align-items-center m-2">
+      <PButton
+          icon="pi pi-github"
+          label="Suggest Fix"
+          class="p-button-sm p-button-text p-button-rounded"
+          @click="createGitHubIssue"
+          severity="secondary"
+      />
+      <PChip class="p-1 m-1" v-tooltip="sourceTooltip(store.selectedObject.Source)">{{ store.selectedObject.Source }}</PChip>
+    </div>
+    <PDivider class="mt-2"/>
     <table>
       <tr v-for="(value, key) in store.selectedObject" :key="key">
         <th v-if="typeof value === 'string' && key !== 'Summary' && key !== 'Source'" class="text-left">{{ key }}: </th>
